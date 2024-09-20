f2e11baf03fcd42097d54b4ceae42598672c81c6
charuka deshan <itscharukadeshan@gmail.com>
2024-09-20 12:03:18 +0530

    config🔧: Remove tests data

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,6 +1,4 @@
 /** @format */
-
-import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -9,7 +7,7 @@ import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 import baseLayerConfig from "../config/baseLayerConfig";
 
-const { BaseLayer, Overlay } = LayersControl;
+const { BaseLayer } = LayersControl;
 
 const DefaultIcon = L.icon({
   iconUrl: icon,
@@ -18,23 +16,6 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map: React.FC = () => {
-  const [mapLayers, setMapLayers] = useState<{ name: string; url: string }[]>(
-    []
-  );
-
-  useEffect(() => {
-    fetch("https://gisapps.nsdi.gov.lk/server/rest/services/?f=pjson")
-      .then((response) => response.json())
-      .then((data) => {
-        const layers = data.services.map((service: { name: string }) => ({
-          name: service.name,
-          url: `https://gisapps.nsdi.gov.lk/server/rest/services/${service.name}/MapServer/tile/{z}/{y}/{x}`,
-        }));
-        setMapLayers(layers);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
   return (
     <MapContainer
       center={[7.8731, 80.7718]}
@@ -71,11 +52,6 @@ const Map: React.FC = () => {
             attribution={baseLayerConfig.Grey.attribution}
           />
         </BaseLayer>
-        {mapLayers.map((layer) => (
-          <Overlay key={layer.name} name={layer.name}>
-            <TileLayer url={layer.url} attribution={`&copy; ${layer.name}`} />
-          </Overlay>
-        ))}
       </LayersControl>
     </MapContainer>
   );
