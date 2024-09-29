47c6919379977e7519b4536b5352e2cad0945559
Charuka Deshan <itscharukadeshan@gmail.com>
2024-09-29 13:05:22 +0530

    config🔧: Add basic structure for search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 /** @format */
 
 import Map from "./component/Map";
+import Search from "./component/Search";
 export default function App() {
   return (
     <>
+      <Search />
       <Map />
     </>
   );
