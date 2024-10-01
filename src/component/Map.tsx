38bd9009611dd60469f463d3fb3e047033126bbc
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-01 15:50:07 +0530

    feat✨: Update nav bar

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -21,7 +21,9 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map: React.FC = () => {
-  const [geoJsonUrls, setGeoJsonUrls] = useState<string[]>([""]);
+  const [geoJsonUrls, setGeoJsonUrls] = useState<string[]>([
+    "https://github.com/itscharukadeshan/map_srilanka_data/blob/main/v1/administrative/geo_json/ds_divisions/ds_colombo_92.geojson",
+  ]);
   const [geoJsonData, setGeoJsonData] = useState<GeoJSONResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [color, setColor] = useState<string>("#3388ff");
@@ -91,101 +93,114 @@ const Map: React.FC = () => {
   };
 
   return (
-    <div>
-      <MapContainer
-        center={[7.8731, 80.7718]}
-        zoom={8}
-        style={{ height: "70vh", width: "100%" }}>
-        <LayersControl>
-          {Object.values(baseLayerConfig).map(
-            ({ url, attribution, key, type }) => (
-              <BaseLayer key={key} name={key} checked={key === "OpenStreetMap"}>
-                {type === "glStyle" ? (
-                  <MapLibreTileLayer url={url} attribution={attribution} />
-                ) : (
-                  <TileLayer url={url} attribution={attribution} />
+    <div className='flex h-screen'>
+      <div className='w-80 bg-gray-900 p-4 overflow-y-auto'>
+        <h2 className='text-xl font-semibold'>GeoJSON Control Panel</h2>
+
+        <div className='space-y-4 mt-4'>
+          <label className='block'>
+            <span className='text-gray-200 font-mono my-2'>GeoJSON URLs:</span>
+            {geoJsonUrls.map((url, index) => (
+              <div key={index} className='flex items-center space-x-2'>
+                <input
+                  type='text'
+                  className='mt-1 block w-full px-4 py-2 input input-bordered'
+                  value={url}
+                  onChange={(e) => {
+                    const newUrls = [...geoJsonUrls];
+                    newUrls[index] = e.target.value;
+                    setGeoJsonUrls(newUrls);
+                  }}
+                  placeholder='Enter GeoJSON URL'
+                />
+                {index === geoJsonUrls.length - 1 && (
+                  <button
+                    onClick={() => setGeoJsonUrls([...geoJsonUrls, ""])}
+                    className='btn btn-primary'>
+                    Add URL
+                  </button>
                 )}
-              </BaseLayer>
-            )
-          )}
-        </LayersControl>
+              </div>
+            ))}
+          </label>
 
-        {geoJsonData && <GeoJSON data={geoJsonData} style={geoJsonStyle} />}
-      </MapContainer>
-      {loading && (
-        <div className='flex justify-center items-center'>
-          <span className='loading loading-bars loading-md'></span>
+          <label className='block w-28'>
+            <span className='text-gray-200'>Color :</span>
+            <input
+              type='color'
+              className='mt-1 block w-full'
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </label>
+
+          <label className='block w-28'>
+            <span className='text-gray-200'>Opacity:</span>
+            <input
+              type='range'
+              min='0.1'
+              max='1'
+              step='0.1'
+              className='mt-1 block w-full'
+              value={opacity}
+              onChange={(e) => setOpacity(Number(e.target.value))}
+            />
+            <span className='text-gray-200'>{opacity}</span>
+          </label>
+
+          <label className='block'>
+            <span className='text-gray-200 font-mono font-bold'>Repo URL:</span>
+            <a
+              href='https://github.com/itscharukadeshan/map_srilanka_data/tree/main/v1/administrative/geo_json'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-500 underline ml-2 font-mono font-bold'>
+              Administrative
+            </a>
+          </label>
+          <label className='block'>
+            <span className='text-gray-200 font-mono font-bold'>Repo URL:</span>
+            <a
+              href='https://github.com/itscharukadeshan/map_srilanka_data/tree/main/v1/forest/geo_json'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-500 underline ml-2 font-mono font-bold'>
+              Forest
+            </a>
+          </label>
         </div>
-      )}
 
-      <div className='p-4 bg-gray-100 space-y-4'>
-        <label className='block'>
-          <span className='text-gray-700'>GeoJSON URLs:</span>
-          {geoJsonUrls.map((url, index) => (
-            <div key={index} className='flex items-center space-x-2'>
-              <input
-                type='text'
-                className='mt-1 block w-full px-4 py-2 input input-bordered input-infoc max-w-xs'
-                value={url}
-                onChange={(e) => {
-                  const newUrls = [...geoJsonUrls];
-                  newUrls[index] = e.target.value;
-                  setGeoJsonUrls(newUrls);
-                }}
-                placeholder='Enter GeoJSON URL'
-              />
-              {index === geoJsonUrls.length - 1 && (
-                <button
-                  onClick={() => setGeoJsonUrls([...geoJsonUrls, ""])}
-                  className='btn btn-primary'>
-                  Add URL
-                </button>
-              )}
-            </div>
-          ))}
-        </label>
-        <label className='block w-fit'>
-          <span className='text-gray-700'>Example url:</span>
-          <a
-            href='https://github.com/itscharukadeshan/map_srilanka_data/blob/main/v1/administrative/geo_json/ds_divisions/ds_colombo_92.geojson'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-blue-500 underline ml-2'>
-            https://github.com/itscharukadeshan/map_srilanka_data/blob/main/v1/administrative/geo_json/ds_divisions/ds_colombo_92.geojson
-          </a>
-        </label>
-        <label className='block w-fit'>
-          <span className='text-gray-700 font-mono font-bold'>Repo url:</span>
-          <a
-            href='https://github.com/itscharukadeshan/map_srilanka_data/tree/main/v1'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-blue-500 underline ml-2 font-mono font-bold'>
-            https://github.com/itscharukadeshan/map_srilanka_data/tree/main/v1
-          </a>
-        </label>
-        <label className='block w-28'>
-          <span className='text-gray-700'>Color:</span>
-          <input
-            type='color'
-            className='mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg'
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-          />
-        </label>
-        <label className='block w-28'>
-          <span className='text-gray-700'>Opacity:</span>
-          <input
-            type='range'
-            min='0.1'
-            max='1'
-            step='0.1'
-            className='mt-1 block w-full'
-            value={opacity}
-            onChange={(e) => setOpacity(Number(e.target.value))}
-          />
-          <span className='text-gray-500'>{opacity}</span>
-        </label>
+        {loading && (
+          <div className='flex justify-center items-center mt-4'>
+            <span className='loading loading-bars loading-md'></span>
+          </div>
+        )}
+      </div>
+
+      <div className='flex-1'>
+        <MapContainer
+          center={[7.8731, 80.7718]}
+          zoom={8}
+          style={{ height: "100%", width: "100%" }}>
+          <LayersControl>
+            {Object.values(baseLayerConfig).map(
+              ({ url, attribution, key, type }) => (
+                <BaseLayer
+                  key={key}
+                  name={key}
+                  checked={key === "OpenStreetMap"}>
+                  {type === "glStyle" ? (
+                    <MapLibreTileLayer url={url} attribution={attribution} />
+                  ) : (
+                    <TileLayer url={url} attribution={attribution} />
+                  )}
+                </BaseLayer>
+              )
+            )}
+          </LayersControl>
+
+          {geoJsonData && <GeoJSON data={geoJsonData} style={geoJsonStyle} />}
+        </MapContainer>
       </div>
     </div>
   );
