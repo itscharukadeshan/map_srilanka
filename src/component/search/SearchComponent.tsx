7692213f5ed23b3613adbb9ddb1397aa7569595d
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-11 14:39:18 +0530

    feat✨: Add aria aria-label and add clear search btn

--- src/component/search/SearchComponent.tsx
+++ src/component/search/SearchComponent.tsx
@@ -1,7 +1,8 @@
 /** @format */
 
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, KeyboardEvent } from "react";
 import Fuse from "fuse.js";
+import { SearchX } from "lucide-react";
 import administrativeData from "../../data/search/administrative_updated_compact.json";
 import createResultObject from "../../services/generateRawUrl";
 
@@ -23,6 +24,7 @@ const fuseOptions = {
 const SearchComponent = () => {
   const [query, setQuery] = useState<string>("");
   const [suggestions, setSuggestions] = useState<Administrative[]>([]);
+  const [highlightedIndex, setHighlightedIndex] = useState<number>(-1);
 
   const fuse = new Fuse(administrativeData as Administrative[], fuseOptions);
 
@@ -32,12 +34,39 @@ const SearchComponent = () => {
 
     if (searchTerm) {
       const result = fuse.search(searchTerm);
-
       const filteredSuggestions = result.map((res) => res.item).slice(0, 5);
       setSuggestions(filteredSuggestions);
     } else {
       setSuggestions([]);
     }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowDown") {
+      setHighlightedIndex((prev) => Math.min(prev + 1, suggestions.length - 1));
+      e.preventDefault();
+    } else if (e.key === "ArrowUp") {
+      setHighlightedIndex((prev) => Math.max(prev - 1, 0));
+      e.preventDefault();
+    } else if (e.key === "Enter") {
+      if (highlightedIndex >= 0) {
+        const selectedItem = suggestions[highlightedIndex];
+        if (selectedItem) {
+          const result = createResultObject(selectedItem);
+          if (result) {
+            console.log(result);
+          }
+        }
+      }
+    } else if (e.key === "Escape") {
+      setSuggestions([]);
+    }
+  };
+
+  const clearInput = () => {
+    setQuery("");
+    setSuggestions([]);
+    setHighlightedIndex(-1);
   };
 
   const getLabel = (type: string) => {
@@ -56,19 +85,32 @@ const SearchComponent = () => {
   };
 
   return (
-    <div className='search-container w-56'>
-      <input
-        type='text'
-        value={query}
-        onChange={handleSearch}
-        placeholder='Search...'
-        className='border p-2 rounded w-full'
-      />
+    <div className='search-container w-56 pl-5'>
+      <div className='flex'>
+        <input
+          type='text'
+          value={query}
+          onChange={handleSearch}
+          onKeyDown={handleKeyDown}
+          placeholder='Search...'
+          className='border p-2 rounded w-full'
+          aria-label='Search Input'
+        />
+        <button
+          onClick={clearInput}
+          aria-label='Clear Search Input'
+          className=' btn btn-warning mx-2 '>
+          <SearchX />
+        </button>
+      </div>
       <ul className='suggestions-list mt-2'>
         {suggestions.map((item, index) => (
           <li
             key={index}
-            className='p-2 border-b cursor-pointer'
+            className={`p-2 border-b cursor-pointer ${
+              highlightedIndex === index ? "bg-gray-200" : ""
+            }`}
+            onMouseEnter={() => setHighlightedIndex(index)}
             onClick={() => {
               const result = createResultObject(item);
               if (result) {
