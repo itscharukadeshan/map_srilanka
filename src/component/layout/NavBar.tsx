a8c599935984f768f4b21225f6bb7cab8c12a2cd
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-11 15:14:12 +0530

    feat✨: Add responsive Hamburger

--- src/component/layout/NavBar.tsx
+++ src/component/layout/NavBar.tsx
@@ -4,37 +4,33 @@ function NavBar({ toggleSidebar }: { toggleSidebar: () => void }) {
   return (
     <div className='navbar bg-base-100'>
       <div className='flex-none'>
-        <button onClick={toggleSidebar} className='btn btn-square btn-ghost'>
+        <label className='btn btn-circle swap swap-rotate'>
+          <input onClick={toggleSidebar} type='checkbox' />
+
           <svg
+            className='swap-off fill-current'
             xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            className='inline-block h-5 w-5 stroke-current'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M4 6h16M4 12h16M4 18h16'></path>
+            width='32'
+            height='32'
+            viewBox='0 0 512 512'>
+            <path d='M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z' />
           </svg>
-        </button>
+
+          <svg
+            className='swap-on fill-current'
+            xmlns='http://www.w3.org/2000/svg'
+            width='32'
+            height='32'
+            viewBox='0 0 512 512'>
+            <polygon points='400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49' />
+          </svg>
+        </label>
       </div>
       <div className='flex-1'>
         <a className='btn btn-ghost text-xl'>Map Srilanka</a>
       </div>
       <div className='flex-none'>
-        <button className='btn btn-square btn-ghost'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            className='inline-block h-5 w-5 stroke-current'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z'></path>
-          </svg>
-        </button>
+        <button className='btn btn-square btn-ghost'></button>
       </div>
     </div>
   );
