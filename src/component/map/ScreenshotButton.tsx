701aa98557ad75215e744d7aac182b73ac23a358
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-12 13:16:44 +0530

    feat✨: Add loading animation and update image naming

--- src/component/map/ScreenshotButton.tsx
+++ src/component/map/ScreenshotButton.tsx
@@ -1,9 +1,11 @@
 /** @format */
-import React from "react";
+import React, { useState } from "react";
 import * as htmlToImage from "html-to-image";
 import { FileDown } from "lucide-react";
 
 const ScreenshotButton: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const takeScreenshot = () => {
     const mapContainer: HTMLElement | null =
       document.querySelector(".leaflet-container");
@@ -11,16 +13,22 @@ const ScreenshotButton: React.FC = () => {
     if (!mapContainer) {
       return;
     } else {
+      setLoading(true);
+
       htmlToImage
         .toPng(mapContainer)
         .then((dataUrl: string) => {
+          const randomNum = Math.floor(Math.random() * 10000);
           const link = document.createElement("a");
           link.href = dataUrl;
-          link.download = "map-screenshot.png";
+          link.download = `map_srilanka-${randomNum}.png`;
           link.click();
         })
         .catch((error: Error) => {
           console.error("Error taking screenshot:", error);
+        })
+        .finally(() => {
+          setLoading(false);
         });
     }
   };
@@ -28,8 +36,13 @@ const ScreenshotButton: React.FC = () => {
   return (
     <button
       className='btn btn-ghost btn-outline btn-success'
-      onClick={takeScreenshot}>
-      <FileDown />
+      onClick={takeScreenshot}
+      disabled={loading}>
+      {loading ? (
+        <span className='loading loading-spinner text-success'></span>
+      ) : (
+        <FileDown />
+      )}
     </button>
   );
 };
