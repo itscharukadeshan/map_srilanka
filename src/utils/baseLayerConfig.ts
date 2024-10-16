ab7f2b043c6634eb7f7f7010ecae16529ad4e274
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-16 22:16:16 +0530

    feat✨: Add site_Attributions to map

--- src/utils/baseLayerConfig.ts
+++ src/utils/baseLayerConfig.ts
@@ -7,136 +7,114 @@ interface BaseLayer {
   key: string;
   type: "raster" | "glStyle" | "vector";
 }
-// var TopPlusOpen_Grey = L.tileLayer(
-//   "http://sgx.geodatenzentrum.de/wmts_topplus_open/tile/1.0.0/web_grau/default/WEBMERCATOR/{z}/{y}/{x}.png",
-//   {
-//     maxZoom: 18,
-//     attribution:
-//       'Map data: &copy; <a href="http://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>',
-//   }
-// );
+
+const site_Attribution = ` | </a> Overlays from <a href="https://gisapps.nsdi.gov.lk/server/rest/services">NSDI Sri Lanka</a> <a href="https://github.com/itscharukadeshan/map_srilanka_data">| Map Srilanka Data</a>`;
+
 const baseLayerConfig: { [key: string]: BaseLayer } = {
   CartoDB_PositronNoLabels: {
     url: "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | contributors &copy; <a href="https://carto.com/attributions">CARTO ${site_Attribution}`,
     key: "Carto Positron",
     type: "raster",
   },
   Carto_DB_Dark_NoLabels: {
     url: "https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>${site_Attribution}`,
     key: "Carto Dark",
     type: "raster",
   },
   Esri_Satellite: {
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    attribution:
-      " &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    attribution: ` &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community${site_Attribution}`,
     key: "Esri Satellite",
     type: "raster",
   },
   Carto_DB_Voyager_No_Labels: {
     url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>${site_Attribution}`,
     key: "Carto Voyager",
     type: "raster",
   },
   openStreetMap: {
     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors${site_Attribution}`,
     key: "OpenStreetMap",
     type: "raster",
   },
 
   humanitarian: {
     url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors ${site_Attribution}`,
     key: "Humanitarian",
     type: "raster",
   },
 
   openTopoMap: {
     url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors${site_Attribution}`,
     key: "Open Topo Map",
     type: "raster",
   },
   TopPlusOpen_Grey: {
-    url: "http://sgx.geodatenzentrum.de/wmts_topplus_open/tile/1.0.0/web_grau/default/WEBMERCATOR/{z}/{y}/{x}.png",
-    attribution:
-      'Map data: &copy; <a href="http://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>',
+    url: `http://sgx.geodatenzentrum.de/wmts_topplus_open/tile/1.0.0/web_grau/default/WEBMERCATOR/{z}/{y}/{x}.png`,
+    attribution: `Map data: &copy; <a href="http://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>${site_Attribution}`,
     key: "Top Plus Grey",
     type: "raster",
   },
   OSM_Bright: {
     url: `https://tiles.stadiamaps.com/styles/osm_bright.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "OSM Bright",
     type: "glStyle",
   },
   OSM_France: {
     url: "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors',
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors${site_Attribution}`,
     key: "OSM France",
     type: "raster",
   },
 
   StadiaSatellite: {
     url: `https://tiles.stadiamaps.com/styles/alidade_satellite.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stadia Satellite",
     type: "glStyle",
   },
   StadiaAlidadeSmoothDark: {
     url: `https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stadia Smooth Dark",
     type: "glStyle",
   },
   StadiaAlidadeSmooth: {
     url: `https://tiles.stadiamaps.com/styles/alidade_smooth.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stadia Smooth",
     type: "glStyle",
   },
 
   StadiaStamenToner: {
     url: `https://tiles.stadiamaps.com/styles/stamen_toner.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stadia Toner",
     type: "glStyle",
   },
   StamenTerrain: {
     url: `https://tiles.stadiamaps.com/styles/stamen_terrain.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stamen Terrain",
     type: "glStyle",
   },
 
   StamenWatercolor: {
     url: `https://tiles.stadiamaps.com/styles/stamen_watercolor.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stamen Watercolor",
     type: "glStyle",
   },
   StamenTerrainBackground: {
     url: `https://tiles.stadiamaps.com/styles/stamen_terrain_background.json?api_key=${STADIA_MAPS_API_KEY}`,
-    attribution:
-      "&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors",
+    attribution: `&copy; Stadia Maps, &copy; OpenMapTiles, &copy; OpenStreetMap contributors${site_Attribution}`,
     key: "Stamen Terrain Background",
     type: "glStyle",
   },
